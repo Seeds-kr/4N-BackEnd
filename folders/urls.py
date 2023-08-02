20d72430fbfc68901a4867066657afced82a1f9c
임서하 <lim021118@gmail.com>
2023-08-03 04:41:04 +0900

    folder 수정

--- folders/urls.py
+++ folders/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('api/folders/', views.get_folders, name='get_folders'),
+    # 다른 URL 매핑들 추가 가능
 ]
--- folders/views.py
+++ folders/views.py
@@ -1,16 +1,15 @@
 from django.http import JsonResponse
-from .models import Folder, Location, FolderLocation
+from .models import Folder
 
 
-def get_folders(request):
+def get_folders(request):   
+    folders = Folder.objects.all()  # 모든 폴더 객체 가져옴
     folder_data = []
-    folders = Folder.objects.all()
     for folder in folders:
-        location_list = folder.locations.all()  # 해당 folder에 속한 locations 가져옴
-        location_names = [location.name for location in location_list]  # 윗줄에서 가져온 장소 목록 'location_list'에서 장소의 이름만을 추출하여 저장
-        folder_data.append({    # 딕셔너리
+        location_names = list(folder.locations.values_list('name', flat=True))  # 장소들의 이름을 리스트로 저장
+        folder_data.append({
             'id': folder.id,
             'name': folder.name,
-            'location': location_names  # JSON 응답에 10줄에서 가져온 장소 이름들이 포함되도록...
+            'location': location_names,
         })
     return JsonResponse(folder_data, safe=False)
